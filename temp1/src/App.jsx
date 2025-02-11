ca382057d9b5a6775442b2aeceb70d2e290be5ff
Pabel <pabel.hernandez@live.com>
2025-02-11 12:20:30 -0600

    update

--- temp1/src/App.jsx
+++ temp1/src/App.jsx
@@ -3,12 +3,18 @@ import { useState } from 'react'
 const App = () => {
   const [ counter, setCounter ] = useState(0)
 
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
+  const handleClick = () => {
+    console.log('clicked')
+  }
 
   return (
-    <div>{counter}</div>
+    <div>
+      <div>{counter}</div>
+      <button onClick={() => setCounter(counter + 1)}>
+        plus
+      </button>
+    </div>
   )
 }
+
+export default App
