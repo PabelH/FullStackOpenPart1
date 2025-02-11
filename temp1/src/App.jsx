cbaf854db0e3240b53ce0ba6c560436e46de7778
Pabel <pabel.hernandez@live.com>
2025-02-11 12:33:12 -0600

    update

--- temp1/src/App.jsx
+++ temp1/src/App.jsx
@@ -1,19 +1,43 @@
 import { useState } from 'react'
 
+const Display = (props) => {
+  return (
+    <div>{props.counter}</div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
+
 const App = () => {
   const [ counter, setCounter ] = useState(0)
+  console.log('rendering with counter value', counter)
 
-  const handleClick = () => {
-    console.log('clicked')
-  }
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
 
   return (
-    <div>
-      <div>{counter}</div>
-      <button onClick={() => setCounter(counter + 1)}>
-        plus
-      </button>
-    </div>
+    <>
+      <Display counter={counter}/>
+      <Button
+        onClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        onClick={setToZero}
+        text='zero'
+      />     
+      <Button
+        onClick={decreaseByOne}
+        text='minus'
+      />
+    </>
   )
 }
 
