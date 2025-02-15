555c09f73071affe1ae47e3cd7ff6624cdae841f
Pabel <pabel.hernandez@live.com>
2025-02-15 07:02:37 -0600

    update

--- temp1/src/App.jsx
+++ temp1/src/App.jsx
@@ -1,54 +1,24 @@
 import { useState } from 'react'
 
-const History = ({allClicks}) => {
-  if (allClicks.length === 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
-  return (
-    <div>
-      button press history: {allClicks.join(' ')}
-    </div>
-  )
-}
 
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
-)
+
+
+
 
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
-  const [total, setTotal] = useState(0)
+  const [value, setValue] = useState(10)
 
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    const updatedLeft = left + 1
-    setLeft(updatedLeft)
-    setTotal(updatedLeft + right)
-
+  const setToValue = (newValue) => () => {
+    console.log('value now', newValue)  // imprime el nuevo valor en la consola
+    setValue(newValue)
   }
 
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
-    setTotal(left + right)
-  }
-  
   return (
     <div>
-      {left}
-      <Button handleClick={handleLeftClick} text='left' />
-      <Button handleClick={handleRightClick} text='right' />
-      {right}
-       
-      <History allClicks={allClicks} />
+      {value}
+      <button onClick={setToValue(1000)}>thousand</button>
+      <button onClick={setToValue(0)}>reset</button>
+      <button onClick={setToValue(value + 1)}>increment</button>
     </div>
   )
 }
